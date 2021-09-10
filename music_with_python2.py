8caeffdb41e2b3c9b8acb81cc3bbd60f5e7ec76d
Bhargav <bhargavkadali39@gmail.com>
2021-09-10 22:04:58 +0530

    Update music_with_python2.py

--- music_with_python2.py
+++ music_with_python2.py
@@ -5,20 +5,20 @@ playback_time=2.1
 print('playback_time :'+str(playback_time))
 
 # Play wav file
-print('Windows Exclamation')
+print('\nWindows Exclamation\n')
 winsound.PlaySound('c:/windows/media/Windows Exclamation.wav', winsound.SND_FILENAME)
 
 # Play sound from control panel settings
-print('SystemQuestion')
+print('SystemQuestion\n')
 winsound.PlaySound('SystemQuestion', winsound.SND_ALIAS)
 
 # Play wav file from memory
-print('Chimes')
+print('Chimes\n')
 data=open('c:/windows/media/Chimes.wav',"rb").read()
 winsound.PlaySound(data, winsound.SND_MEMORY)
 
 # Start playing the first bit of wav file asynchronously
-print('Windows Proximity Connection')
+print('Windows Proximity Connection\n')
 winsound.PlaySound('c:/windows/media/Windows Proximity Connection.wav',
 winsound.SND_FILENAME|winsound.SND_ASYNC)
 
@@ -28,6 +28,7 @@ time.sleep(playback_time)
 # Before stopping it
 winsound.PlaySound(None, 0)
 
+print('Thank you')
 
 # C:\Windows\Media
 # Copy above path for windows predefined audio files that you can use
